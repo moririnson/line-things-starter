d9eb8e8ae288ac95088f4eb8f592774ea897cf7d
morimorim <mori.shunsuke@linecorp.com>
2019-06-10 13:36:24 +0900

    Update service uuid

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '2a3ae474-3502-4309-9816-27c7d98ff88d'; // LED, Button
+const USER_SERVICE_UUID         = '96a87ce5-eea3-44b2-a755-7ca0f3acdc07'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
